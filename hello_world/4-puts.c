1116dbadd9f3033970158ac9815d4c405d32a777
zabalae <elizabeth.zabala3@gmail>
2023-10-11 16:51:07 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-
+/**
+ * main - Entry point of the code
+ *
+ * Return - 0 Successful
+ */
 int main(void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	
 	return 0;
 }
