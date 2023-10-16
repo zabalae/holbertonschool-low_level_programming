b19749c91aa9584bb48e148e54ade9b2d230befa
zabalae <elizabeth.zabala3@gmail>
2023-10-16 07:25:58 -0700

    last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,6 +11,7 @@ int main(void)
 {
 	int n;
 	int last_digit;
+
 	srand(time(0));
 				
 	n = rand() - RAND_MAX / 2;
@@ -25,7 +26,7 @@ int main(void)
 	{
 		printf("The last digit of %d is %d and is 0\n", n, last_digit);
 	}
-	else if (last_digit < 6)||(last_digit != 0)
+	else if ((last_digit < 6)||(last_digit != 0))
 	{
 		printf("The lat digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	}
