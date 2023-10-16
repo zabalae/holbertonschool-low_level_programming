d9e2995fa352385e48b20c359134b616b9be925a
zabalae <elizabeth.zabala3@gmail>
2023-10-16 09:01:20 -0700

    Print alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,19 +11,19 @@ int main(void)
 	char lower;
 	char upper;
 
-	for(lower = 'a'; lower <= 'z'; lower++)
+	for (lower = 'a'; lower <= 'z'; lower++)
 	{
 		putchar(lower);
 	}
 
-	for(upper = 'A'; upper <= 'Z'; upper++)
+	for (upper = 'A'; upper <= 'Z'; upper++)
 	{
 		putchar(upper);
 	}
 
 	putchar('\n');
 
-	return(0);
+	return (0);
 
 
 }
