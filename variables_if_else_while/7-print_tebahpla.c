874b35212de77b9a627954c1480bea88936c3c06
zabalae <elizabeth.zabala3@gmail>
2023-10-16 10:26:08 -0700

    lowercase alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- Prints alphabet in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x;
+	for (x = 'z'; x <= 'a'; x--)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
