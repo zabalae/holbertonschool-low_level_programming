534ec2a40a5b4b03c1bbc533b9a87712bf1f7b4e
zabalae <elizabeth.zabala3@gmail>
2023-10-16 09:20:10 -0700

    print alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main- print alphabeth except q and e
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'e')
+		{
+			a++;
+		}
+		else if (a == 'q')
+		{
+			a++;
+		}
+
+		putchar ('\n');
+
+		return (0);
+	}
+}
