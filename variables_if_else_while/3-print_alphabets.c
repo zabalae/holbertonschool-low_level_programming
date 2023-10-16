cc091e6eb212a1b805bc8523901c7eb915737261
zabalae <elizabeth.zabala3@gmail>
2023-10-16 08:26:57 -0700

    Print alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main- Prints alphabet in lowercase and uppercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lower;
+	char upper;
+
+	for(lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+
+	for(upper = 'A'; upper <= 'A'; upper++)
+	{
+		putchar(upper);
+	}
+
+	putchar ('\n');
+
+	return(0);
+
+
+}
