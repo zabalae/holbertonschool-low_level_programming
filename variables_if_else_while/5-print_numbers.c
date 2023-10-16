9d174a775e6a0cc30424c65142ff9b52deb06859
zabalae <elizabeth.zabala3@gmail>
2023-10-16 09:53:38 -0700

    Print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Prints single digit numbers of base 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf ("%d ,", x);
+	}
+
+	printf('\n');
+
+	return (0);
+}
