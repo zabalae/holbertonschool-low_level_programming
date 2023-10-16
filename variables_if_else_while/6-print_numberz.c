12074d840264f9b2efb7cd08a46e806485fe3e3c
zabalae <elizabeth.zabala3@gmail>
2023-10-16 10:12:15 -0700

    Print numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Printsall single digit numbers of base 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for(x = 0; x <= 9, x++)
+	{
+		putchar('0' + x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
