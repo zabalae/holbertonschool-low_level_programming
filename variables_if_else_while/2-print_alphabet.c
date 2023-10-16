41a1657ca3908c1a8a696ef84bffd5eafee25b7d
zabalae <elizabeth.zabala3@gmail>
2023-10-16 08:03:20 -0700

    Print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Print alphabet letters
+ *
+ * Return: 0 Succesful
+ */
+
+int main (void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return:(0);
+}
