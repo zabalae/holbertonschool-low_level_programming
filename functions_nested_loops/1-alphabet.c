fdd73b5b93a57ba6def7b8066f8b0d22693e8fa0
zabalae <elizabeth.zabala3@gmail>
2023-10-17 07:18:12 -0700

    print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * main- print alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char x;
+	for(x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#include "_putchar.c"
+
+int main(void)
+{
+	void print_alphabet(void);
+}
