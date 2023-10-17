d26c2596d280e55f5d256ffd36a2643e881b70b7
zabalae <elizabeth.zabala3@gmail>
2023-10-17 09:45:18 -0700

    alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- Prints alphabet 10 times
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int num = 1;
+
+	while (num <=10)
+	{
+		char x;
+
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+
+			_putchar('\n');
+		}
+	}
+}
