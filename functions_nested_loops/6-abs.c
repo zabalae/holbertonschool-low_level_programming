399704930856d85bed7144e2f7065b99320a3705
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-18 14:15:38 -0400

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of integer
+ *
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	int x;
+
+	if (x < 0)
+	{
+		x = x * -1;
+
+		_putchar(x);
+	}
+
+	else
+	{
+		_putchar(x);
+	}
+}
