16b91c4048e8dac22dc85d64d2eb7740f2a2e9f9
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-18 07:13:50 -0400

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * print_sign- Prints the sign of a number
+ * print_sign - Prints the sign of a number
  *
  * n: variable
  *
@@ -13,22 +13,22 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar (43);
-		
+
 		return (1);
 	}
 
-	else if (n ==0)
+	else if (n == 0)
 	{
 		_putchar(48);
 
 		return (0);
 	}
-	else if (n <0)
+	else if (n < 0)
 	{
 		_putchar(45);
 
 		return (-1);
 	}
-	
+
 	return (0);
 }
