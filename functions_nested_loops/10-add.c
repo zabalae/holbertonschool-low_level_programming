a70ac244561e8a4a7060076aa648e2569c78dcac
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-19 03:53:30 -0400

    a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add- adds two integers
+ *
+ * @x: first number
+ *
+ * @y: second number
+ */
+
+int add(int x, int y)
+{
+	return (x + y)
+}
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * jack_bauer- prints every minute of the day of Jack Bauer
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+
+}
