bfb92fa08db8f4ef21476592a50f4f4f57f2527d
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-19 13:14:32 -0400

    more numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers- prints 10 times the numbers from 0 to 14
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y = 0;
+
+	while ( y == 10)
+	{
+
+		for (x = 0; x <= 14; x++)
+		{
+			if (x >= 10)
+			{
+				_putchar((x%10) + '0');
+			}
+
+			_putchar(x + '0');
+		}
+
+		_putchar('\n');
+
+		y++
+	}
+
+}
