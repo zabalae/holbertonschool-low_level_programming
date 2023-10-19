4e5e7849d375189c3522a470a733c06d5dd71f50
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-19 12:56:55 -0400

    print most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- prints the numbers from 0 to 9 except 2 and 4
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		if (x == 50) || (x == 52)
+		{
+			x++;
+		}
+
+		_putchar(x);
+	}
+
+	_putchar('\n');
+}
