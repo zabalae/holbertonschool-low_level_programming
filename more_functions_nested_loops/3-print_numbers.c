692cee6b2ab5ad1efc2953f3a4f82a2dffb7615b
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-19 12:45:28 -0400

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar("\n");
+
+	return (0);
+}
