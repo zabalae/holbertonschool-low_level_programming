ec00deafccf01b15cdb47f6b238bf677710dc95e
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-19 22:04:36 -0400

    24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,5 +8,23 @@
 
 void jack_bauer(void)
 {
+	int x, y, i, z;
 
+	for (x = 0; x <= 2; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			if ((x <= 1 && y <= 9) || (x <= 2 && y <= 3))
+			{
+				for (i = 0; i <= 5; i++)
+				{
+					_putchar(x + '0');
+					_putchar(y + '0');
+					_putchar(58);
+					_putchar(i + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
