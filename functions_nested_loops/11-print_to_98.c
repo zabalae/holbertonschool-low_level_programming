3d3670841db0202db6109d9c01126354d56a4ac3
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-20 05:01:34 -0400

    print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -26,20 +26,26 @@ void print_to_98(int n)
 
 			else
 			{
-				for (; n >= 98; n--)
-				{
-					if (n == 98)
-					{
-						printf("%d", n);
-						printf("\n");
-						
-						break;
-					}
-					else
-					{
-						printf("%d, ", n);
-					}
-				}
+				printf("%d, ", n);
+			}
+		}
+	}
+
+	else
+	{
+		for (; n >= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf('\n');
+
+				break;
+			}
+
+			else
+			{
+				printf("%d, ", n);
 			}
 		}
 	}
