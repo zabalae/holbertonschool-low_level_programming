dd42330c1846093f0ffa0101846a1e5dfa98130e
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-19 21:46:51 -0400

    times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for(b = 0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
 
@@ -20,7 +20,7 @@ void times_table(void)
 			{
 				d = c % 10;
 
-				e = (c - e) / 10;
+				e = (c - d) / 10;
 
 				_putchar(44);
 				_putchar(32);
