716ac77ff814448fb0cd97de845ea3d2338f3090
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-21 12:57:22 -0400

    print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_aquare - function that prints a square
+ *
+ * @size: size of the square
+ *
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (y = 0; y < size; y++)
+		{
+			if (y == x)
+
+			for (x = 0; x < size; x++)
+			{
+				_putchar(35);
+
+				break;
+			}
+
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
