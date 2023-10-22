21e8134695a683db544a31150ae3d11ce4ccced5
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-22 19:55:12 -0400

    Print triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangles - function that prints a triangle
+ *
+ * @size: the size of the triangle
+ *
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = size - x; y >= 1; y--)
+			{
+				_putchar(' ');
+			}
+
+			for (z = 1; y <= x; z++)
+			{
+				_putchar('35');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
