12b0d39ce0d3fb9cc58e7f29ca551d1705b5b7e3
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-22 19:07:26 -0400

    Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0) && (x % 5 == 0)
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz ");
 
@@ -41,4 +41,6 @@ int main(void)
 
 		printf("%d, ", x);
 	}
+
+	return (0);
 }
