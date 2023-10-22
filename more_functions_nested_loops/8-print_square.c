fe92e64826aba3ad23616e296a02d31ac6838991
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-22 18:23:06 -0400

    print square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -12,32 +12,17 @@ void print_square(int size)
 {
 	int x, y;
 
-	if (size > 0)
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (x = 0; x < size; x++)
 	{
 		for (y = 0; y < size; y++)
 		{
-			for (x = 0; x < size; x++)
-			{
-				if (y == x)
-				{
-					_putchar(35);
-
-					break;
-				}
-			
-
-				else
-				{
-					_putchar('\n');
-				}
-			}
-
-			_putchar('\n');
+			_putchar(35);
 		}
-	}
-
-	else
-	{
 		_putchar('\n');
 	}
 }
