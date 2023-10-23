8448f6313769265592500e48d25aa01985657308
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-23 17:21:19 -0400

    rev string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ *
+ * @s: string
+ *
+ * return: 0
+ */
+
+void rev_string(char *s)
+{
+	int x, y;
+
+	char *i = s;
+
+	for (x = 0; *i != '\0'; x++)
+	{
+		*i++;
+	}
+
+	for (y = x - 1; y >= 0; y--)
+	{
+		int z = 0;
+
+		*i[z] = s[y];
+
+		z++;
+	}
+
+	s = i;
+
+	_putchar('\n');
+}
