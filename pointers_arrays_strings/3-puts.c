ac4c991b898a80553c7c8b060fcc714400cb0175
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-23 10:13:25 -0400

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts- function that prints a string
+ *
+ * @str: string
+ *
+ * return: 0
+ */
+
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(str[x];
+	}
+
+	_putchar('\n');
+}
