975eaaf8dc54c44d0e0ee8aa6d5022623652dd19
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-24 15:44:38 -0400

    puts 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ *
+ * @str: - string
+ *
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int x, y;
+
+	for (x = 0; *str != '\0'; x++)
+	{
+		s++;
+	}
+
+	for (y = 0; y < x; y = y + 2)
+	{
+		_putchar(str[y]);
+	}
+
+	_putchar('\n');
+}
