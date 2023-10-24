0bac73ba00ecf5e81de5fca40b0b4b71ffef2dc1
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-24 15:49:44 -0400

    puts 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -12,6 +12,8 @@ void puts2(char *str)
 {
 	int x, y;
 
+	char *i = str;
+
 	for (x = 0; *str != '\0'; x++)
 	{
 		str++;
@@ -19,7 +21,7 @@ void puts2(char *str)
 
 	for (y = 0; y < x; y = y + 2)
 	{
-		_putchar(str[y]);
+		_putchar(i[y]);
 	}
 
 	_putchar('\n');
