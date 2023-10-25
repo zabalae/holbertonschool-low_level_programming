5b6d280367c39ceb7aff5441b667f36899d76f7b
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-25 08:44:10 -0400

    rev string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints half of a string
+ *
+ * @str: string
+ *
+ * return: 0
+ */
+
+void puts_half(char *str)
+{
+	int x,y;
+
+	char *i = str;
+
+	for (x = 0; i != '\0'; x++)
+	{
+		i++;
+	}
+}
