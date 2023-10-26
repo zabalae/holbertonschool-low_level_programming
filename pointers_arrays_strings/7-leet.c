53f4b8b052c0336cd82bb6d5dc5119c2bace4e5a
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-26 19:44:57 -0400

    leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ *
+ * @str: string to encode
+ *
+ * Return: the resulting string
+ */
+
+char *leet(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (str[x] == 'a' || str[x] == 'A')
+		{
+			str[x] = '4';
+		}
+		
+		else if (str[x] == 'e' || str[x] == 'E')
+		{
+            		str[x] = '3';
+		}
+
+        	else if (str[x] == 'o' || str[x] == 'O')
+		{
+            		str[x] = '0';
+		}
+
+        	else if (str[x] == 't' || str[x] == 'T')
+		{
+            		str[x] = '7';
+		}
+
+        	else if (str[x] == 'l' || str[x] == 'L')
+		{
+            		str[x] = '1';
+		}
+	}
+}
