08884cf7b99bcb5999d25b89a68ea7ac8aaa5df1
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-26 10:24:50 -0400

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -16,31 +16,20 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int x = 0;
 	int y = 0;
-	int i, count, z, count1;
-
-	while (src[x] != '\0')
-	{
-		x++;
-	}
 
 	while (dest[y] != '\0')
 	{
 		y++;
 	}
-	z = n + y + 1;
-	count = y;
-	count1 = 0;
 
-	if (n > 0)
+	while (src[x] != '\0' && x < n)
 	{
-
-		for (i = y; i <= z; i++)
-		{	
-			dest[count] = src[count1];
-			count++;
-			count1++;
-		}
+		dest[y] = src[x];
+		y++;
+		x++;
 	}
+
+	dest[y] = '\0';
 
 	return (dest);
 }
