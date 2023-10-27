ed6b98d006c42f4045972eb91b741c2acada251a
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-27 15:51:44 -0400

    strchr

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ *
+ * @s: the main string to be scanned
+ *
+ * @accept: The string containing the list of characters to match in s
+ *
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x;
+
+	for (x = 0; *s[x] != *accept[x]; x++)
+	{
+
+	}
+}
---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard
+ *
+ * @a: array
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+}
