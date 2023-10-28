ac5e65471b3c0cd1aefe29952aa14c9001c04dc8
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-28 12:09:26 -0400

    strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -17,7 +17,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (x = 0; *s != '\0'; x++)
 	{
-		for (y = 0; accept[j] != '\0'; y++)
+		for (y = 0; accept[y] != '\0'; y++)
 		{
 			if (*s == accept[y])
 			{
