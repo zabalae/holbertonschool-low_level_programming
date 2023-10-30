cf866b983a5fc49b31c4004e1282b1037459528f
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-30 18:28:25 -0400

    factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -5,18 +5,23 @@
  *
  * @n: int
  *
- * Return: factorial 
+ * Return: factorial
  */
 
 int factorial(int n)
 {
 	if (n == 0)
 	{
-		return 1;
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		return (-1);
 	}
 
 	else
 	{
-		return n * factorial(n - 1);
+		return (n * factorial(n - 1));
 	}
 }
