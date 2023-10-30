dd4c1ce4676515559be44281d99da3c7922d086d
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-30 16:55:40 -0400

    strlen

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -10,14 +10,13 @@
 
 int _strlen_recursion(char *s)
 {
-	int x = 0;
 
 	if (*s == '\0')
 	{
 		return (x);
 	}
 
-	x = _strlen_recursion(s + 1);
+	int x = _strlen_recursion(s + 1);
 
 	return (x);
 }
