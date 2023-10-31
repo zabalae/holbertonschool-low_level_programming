dbf0d1e586d635945631f423e020db64d552bfdd
Elizabeth <elizabeth.zabala3@gmail.com>
2023-10-31 11:18:58 -0400

    pow recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -19,11 +19,11 @@ int _pow_recursion(int x, int y)
 
 	else if (y > 0)
 	{
-		return x * _pow_recursion(x, y - 1);
+		return (x * _pow_recursion(x, y - 1));
 	}
 
 	else
 	{
-		return (1.0 / (x * _pow_recursion(x, -y -1));
+		return (1.0 / (x * _pow_recursion(x, -y -1)));
 	}
 }
