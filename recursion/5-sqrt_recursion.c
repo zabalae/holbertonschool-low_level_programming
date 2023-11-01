d45ac282568639e00938e1606fd6fdc8a4b1b5e9
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-01 10:41:46 -0400

    sqrt recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,6 +8,29 @@
  * Return: natural square root
  */
 
+int find_sqrt(int left, int right, int n) 
+{
+	int mid = left + (right - left) / 2;
+
+	if ((mid * mid) == n)
+	{
+		return (mid);
+	}
+
+	if (left >= right - 1) {
+		return (-1);
+	}
+
+	if (mid * mid < n)
+	{
+		return (find_sqrt(mid, right, n));
+	}
+	else
+	{
+		return (find_sqrt(left, mid, n));
+	}
+}
+
 int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
@@ -23,30 +46,5 @@ int _sqrt_recursion(int n)
 	else
 	{
 		return (find_sqrt(1, n, n));
-	}
-}
-
-int find_sqrt(int left, int right, int n)
-{
-	int mid = left + (right - left) / 2;
-
-	if ((mid * mid) == n)
-	{
-		return (mid);
-	}
-
-	if (left >= right - 1)
-	{
-		return (-1);
-	}
-
-	if (mid * mid < n)
-	{
-		return (find_sqrt(mid, right, n));
-	}
-
-	else
-	{
-		return (find_sqrt(left, mid, n));
 	}
 }
