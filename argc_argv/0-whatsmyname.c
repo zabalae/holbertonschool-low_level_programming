e40d5a6c98be948a625a065dd5c459b2b3c76a3e
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-04 16:40:20 -0400

    whats my name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,6 +3,12 @@
 /**
  * main - program that prints its name
  *
+ * @argc: int that represents the number of command line arguments
+ *
+ * argv: array of strings that stores the actual command line arguments
+ *
+ * Return: program name
+ *
  */
 
 int main(int argc, char *argv[]) 
@@ -12,14 +18,12 @@ int main(int argc, char *argv[])
 	{
 		for (x  = 0; argv[0][x] != '\0'; x++)
 		{
-
-		_putchar(argv[0][x]);
-		
+		_putchar(argv[0][x]);		
 		}
 
 	_putchar('\n');
 
 	}
 
-	return 0;
+	return (0);
 }
