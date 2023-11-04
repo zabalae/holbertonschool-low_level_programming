25fa002eb29574f7631f95c19cdf19e441fc2c96
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-04 19:28:19 -0400

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main- prints the number of arguments passed into it
+ *
+ * @argc: int that represents the number of command-line arguments
+ *
+ * @argv: array of strings that stores the actual command-line arguments
+ *
+ * Return: the number of arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = argc - 1;
+
+	while (count > 0)
+	{
+		int digit = count % 10;
+		_putchar('0' + digit);
+		count /= 10;
+	}
+
+	putchar('\n');
+
+	return 0;
+}
