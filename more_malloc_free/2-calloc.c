3e917c4f0869088f95e9350eb224a970ed44863d
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-10 10:38:33 -0400

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,22 +15,23 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int x;
 
-	p = (void *) malloc(nmemb * size);
-
 	if ((nmemb == 0) || (size == 0))
 	{
 		return (NULL);
 	}
 
-	if (p)
-	{
-		for (x = 0; x <= nmemb * size; x++)
-		{
-			*(p + x) = 0;
-		}
+	p = malloc(nmemb * size);
 
-		return (p);
+	if ( p == NULL)
+	{
+		return (NULL);
 	}
 
-	return (NULL);
+	for (x = 0; x <= nmemb * size; x++)
+	{
+		*(p + x) = 0;
+	}
+
+	return (p);
+
 }
