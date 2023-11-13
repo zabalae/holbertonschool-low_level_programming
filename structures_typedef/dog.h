cc4e55eca2980cd26588181089ef2604f4dbea3d
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-13 14:04:52 -0400

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure for dog name, age and owner
+ */
+
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
