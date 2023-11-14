d58d27d3a09258a0f1d131f48f142e64a0acebcd
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-14 15:48:21 -0400

    print name

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
