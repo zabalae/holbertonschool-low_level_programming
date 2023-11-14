f0a5bf013d0db30f13c0bd8ba4e32e635fec72f7
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-14 10:11:48 -0400

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,60 +15,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 
 	if (name == NULL || owner == NULL)
-	{
 		return (NULL);
-	}
-
 	d = malloc(sizeof(dog_t));
-
 	if (d == NULL)
-	{
 		return (NULL);
-	}
-
 	for (n = 0; name[n]; n++)
-	{
 		;
-	}
-
 	n++;
-
 	d->name = malloc(n * sizeof(char));
-
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-
 	for (x = 0; x < n; x++)
-	{
 		d->name[x] = name[x];
-	}
-
 	d->age = age;
-
 	for (o = 0; owner[o]; o++)
-	{
 		;
-	}
-
 	o++;
-
 	d->owner = malloc(o * sizeof(char));
-
 	if (d ->owner == NULL)
 	{
 		free(d->name);
 		free(d);
 		return(NULL);
 	}
-
 	for (x = 0; x < o; x++)
-	{
 		d->owner[x] = owner[x];
-	}
-
 	return(d);
-
 }
