09a9349b63e793f39c83e886beb2213ce59f8ee6
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-17 14:06:11 -0400

    sum them all

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: numbers
+ * Return: sum of all parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int x, sum;
+	va_list list;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(list, n);
+
+	for (x = 0; x < n; x++)
+	{
+		sum += va_arg(list, int);
+	}
+
+	va_end(list);
+
+	return (list);
+}
