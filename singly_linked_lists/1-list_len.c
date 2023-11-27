d36822a4eb303fb94565ae5f2d2d3cd170a5bca0
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-27 17:29:14 -0400

    fixing betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - function that returns the number of elements in a 
+ * list_len - function that returns the number of elements in a
  * linked list_t list
  * @h: list
  * Return: number of elements
