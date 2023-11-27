e032c6b3f0cc72193aa9631e45e5539d2fc2e584
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-27 17:27:39 -0400

    list len

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list_t list
+ * @h: list
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	int x;
+
+	for (x = 0; h != NULL; x++)
+	{
+		h = h->next;
+	}
+
+	return (x);
+}
