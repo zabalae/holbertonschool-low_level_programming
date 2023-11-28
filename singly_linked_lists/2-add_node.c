f0115f71677fed8d3fcd1f161b75d3965d6935ea
Elizabeth <elizabeth.zabala3@gmail.com>
2023-11-28 11:18:54 -0400

    add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node at the beginning
+ * of a list_t list
+ * @head: head of the list
+ * @str: string to duplicate
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int x;
+
+	new = malloc(sizeof(list_t));
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		;
+	}
+
+	new->len = x;
+	new->next = *head;
+	*head = new;
+
+	return (*new);
+}
