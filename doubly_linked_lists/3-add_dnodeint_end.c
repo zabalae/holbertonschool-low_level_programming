11cb7b31e6c956a3ec7ae5fabc8bb2b633dfa1c2
Elizabeth <elizabeth.zabala3@gmail.com>
2023-12-01 15:19:31 -0400

    add dnodeint end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,7 +10,7 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t = *new_node, *temp_node;
+	dlistint_t *new_node, *temp_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 
