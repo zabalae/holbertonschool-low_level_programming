0536fee91709ffe30c578f5c4f7a48db5947718a
Elizabeth <elizabeth.zabala3@gmail.com>
2023-12-02 23:54:00 -0400

    free dlistint

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function that frees a dlistint.
+ * @head: head of the list
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+
+	free_dlistint(head->next;
+
+	free(head);
+}
