f73a7e71a813a464049b7150182201026139f494
Elizabeth <elizabeth.zabala3@gmail.com>
2023-12-03 17:23:01 -0400

    Task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,7 +13,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (head == NULL)
 		return (-1);
 
-	dlistint_t *temp = *head;
+	dlistint_t *temp = head;
 
 	while (index > 0 && temp != NULL)
 	{
