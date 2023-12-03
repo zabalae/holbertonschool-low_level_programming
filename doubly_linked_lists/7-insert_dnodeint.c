1ece02fb2393a8f731c7eddd089fa822a8f977e1
Elizabeth <elizabeth.zabala3@gmail.com>
2023-12-03 15:59:02 -0400

    Task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -6,6 +6,7 @@
  * @h: head of the list
  * @idx: index of the list where the new node should be added
  * @n: value of new node
+ * Return: the address of the new node, or NULL if it failed
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
