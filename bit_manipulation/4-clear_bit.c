8b1a1dc66d148a496500758498a3c73f95783afc
Elizabeth <elizabeth.zabala3@gmail.com>
2023-12-03 21:23:40 -0400

    Task 4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -10,14 +10,16 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask = 0;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
 
-	*n -= (*n >> index) & 1;
-	*n >>= 1;
-	*n <<= index;
+	mask = ~(1 << index);
+
+	*n &= mask;
 	
 	return (1);
 }
