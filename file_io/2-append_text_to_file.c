02fd45125ee8ce401b8329a133113f50dc8c71f6
Elizabeth <elizabeth.zabala3@gmail.com>
2023-12-09 09:39:54 -0400

    Task 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function that appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ * Return: 1 on success and -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *file;
+	if (filename == NULL)
+		return (-1);
+	
+	if (text_content == NULL)
+		return (1);
+
+	file = fopen(filename, "a");
+
+	if (file == NULL)
+		return (-1);
+
+	while (*text_content != '\0')
+	{
+		_putchar(*text_content);
+		fputc(*text_content++, file);
+	}
+
+	fclose(file);
+	return (1);
+
+}
