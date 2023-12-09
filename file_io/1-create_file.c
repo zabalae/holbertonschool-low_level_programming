10fca3c67f990a51468bfa40869457c44c86e592
Elizabeth <elizabeth.zabala3@gmail.com>
2023-12-09 00:25:16 -0400

    Task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-		return(-1);
+		return (-1);
 
 	if (text_content != NULL)
 	{
@@ -40,6 +40,6 @@ int create_file(const char *filename, char *text_content)
 	if (chmod(filename, 0600) == -1)
 		return (-1);
 
-	return(1);
-	
+	return (1);
+
 }
