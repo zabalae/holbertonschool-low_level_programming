86d4b4f1841e1b135f127070ea5b386425927ccf
Elizabeth <elizabeth.zabala3@gmail.com>
2023-12-10 20:13:25 -0400

    Task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,14 +13,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0')
-        	return (0);
+		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 
 	if (ht->array[index] == NULL)
 	{
-        	ht->array[index] = malloc(sizeof(hash_node_t));
+		ht->array[index] = malloc(sizeof(hash_node_t));
 		if (ht->array[index] == NULL)
-            		return (0);
+			return (0);
 		ht->array[index]->key = strdup(key);
 		ht->array[index]->value = strdup(value);
 		ht->array[index]->next = NULL;
