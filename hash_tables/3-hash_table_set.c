d3a3dedf7fdaefcb44b13bbf0c42e99ff70fe8ee
Elizabeth <elizabeth.zabala3@gmail.com>
2023-12-10 22:24:40 -0400

    Task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		tmp = ht->array[index];
-		while (tmp->next != NULL)
+		while (tmp != NULL)
 		{
 			if (strcmp(tmp->key, key) == 0)
 			{
