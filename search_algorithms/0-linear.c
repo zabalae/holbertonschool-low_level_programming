e20246c64cc02a5872a7371eb590b8a21c5b0dc5
Elizabeth <elizabeth.zabala3@gmail.com>
2024-01-16 16:45:21 -0400

    Task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	}
 
-	for (x = 0; x < size; x++)
+	for (x = 0; x < (int)size; x++)
 	{
 		printf("Value checked array[%d] = [%d]\n", x, array[x]);
 
