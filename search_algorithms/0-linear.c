5e4f626003cefb4b0551257c4620fd581c09a844
Elizabeth <elizabeth.zabala3@gmail.com>
2024-01-16 16:46:51 -0400

    Task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,6 +4,7 @@
  * linear_search - function that searches for a value in an array
  * of integers using the linear search algorithm
  * @array: pointer to the first element of the array to search in
+ * @size: Number of elements in array
  * @value: the value to search for
  * Return: the first index where value is located
  */
